ff8fb329bbe8d00f5162211c33eedd635e8d1022
Artyom ILYIN <artyom_ilyin@mail.ru>
2021-02-13 20:11:53 +0300

    refactor

--- run.py
+++ run.py
@@ -11,9 +11,11 @@ OUTPUT_FILENAME_FORMAT = '{%номер%}_{%месяц%}_{%имя%}.xlsx'
 filename = os.path.join(TEMPLATE_FOLDER, TEMPLATE_FILENAME)
 
 
-def fill_template(context, template_sheet):
+def fill_template(context, wb):
+    sheet = wb.worksheets[0]
+
     output_filename = OUTPUT_FILENAME_FORMAT
-    for row in template_sheet:
+    for row in sheet:
         for cell in row:
             for key, value in context.items():
                 if key in str(cell.value):
@@ -22,18 +24,17 @@ def fill_template(context, template_sheet):
     output_filename_full = os.path.join(OUTPUT_FOLDER, output_filename)
     wb.save(filename=output_filename_full)
 
-wb = load_workbook(filename=filename)
-
-sheet = wb.worksheets[0]
-context = {
-    '{%номер%}': '123',
-    '{%имя%}': 'Иванов Иван Сергеевич',
-    '{%лицевой_счет%}': '123-123',
-    '{%месяц%}': 'Февраль',
-    '{%год%}': '2021',
-    '{%долг%}': '4043,00',
-    '{%долг_рубли%}': '4043',
-    '{%долг_копейки%}': '00',
-}
-fill_template(context, sheet)
+def read_statement():
+    wb = load_workbook(filename=filename)
+    context = {
+        '{%номер%}': '123',
+        '{%имя%}': 'Иванов Иван Сергеевич',
+        '{%лицевой_счет%}': '123-123',
+        '{%месяц%}': 'Февраль',
+        '{%год%}': '2021',
+        '{%долг%}': '4043,00',
+        '{%долг_рубли%}': '4043',
+        '{%долг_копейки%}': '00',
+    }
+    fill_template(context, wb)
 
