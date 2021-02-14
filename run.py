415f517478dc7d5dc0e5847152d55c22b0276bc6
Artyom ILYIN <artyom_ilyin@mail.ru>
2021-02-14 14:41:31 +0300

    refactor

--- run.py
+++ run.py
@@ -4,11 +4,7 @@ from openpyxl import load_workbook
 from openpyxl.utils import column_index_from_string
 
 
-
-
 CONFIG_NAME = "настройки.txt"
-
-
 
 
 class BillGenerator:
@@ -22,9 +18,8 @@ class BillGenerator:
     def get_col_index(letter):
         return column_index_from_string(letter) - 1
 
-    @staticmethod
-    def fill_template(context, wb):
-        sheet = wb.worksheets[0]
+    def fill_template(self, context):
+        sheet = self.template_wb.worksheets[0]
 
         output_filename = OUTPUT_FILENAME_FORMAT
         for row in sheet:
@@ -34,7 +29,7 @@ class BillGenerator:
                         cell.value = cell.value.replace(key, str(value))
                     output_filename = output_filename.replace(key, str(value))
         output_filename_full = os.path.join(OUTPUT_FOLDER, output_filename)
-        wb.save(filename=output_filename_full)
+        self.template_wb.save(filename=output_filename_full)
 
     @classmethod
     def is_valid(cls, row):
@@ -45,11 +40,11 @@ class BillGenerator:
             return True
         return False
 
+    def __init__(self, template_wb):
+        self.template_wb = template_wb
+        self.bill_data = []
 
-    @classmethod
-    def read_statement(cls):
-
-        statement_files = [file for file in os.listdir(STATEMENT_FOLDER) if file.endswith(".xls") or file.endswith(".xlsx")]
+        statement_files = [file for file in os.listdir(STATEMENT_FOLDER) if file.endswith(".xlsx")]
         try:
             statement_filename = statement_files[0]
         except IndexError:
@@ -58,47 +53,53 @@ class BillGenerator:
         statement_filename_full = os.path.join(STATEMENT_FOLDER, statement_filename)
 
         statement = load_workbook(filename=statement_filename_full, data_only=True).worksheets[0]
-        result = []
         for row_index in range(FIRST_ROW, LAST_ROW + 1):
             row = statement[row_index]
-            if cls.is_valid(row):
+            if self.is_valid(row):
                 context = {
-                    '{%номер%}': row[cls.get_col_index(cls.StatementCols.NUMBER)].value,
-                    '{%имя%}': row[cls.get_col_index(cls.StatementCols.NAME)].value,
-                    '{%лицевой_счет%}': row[cls.get_col_index(cls.StatementCols.ACCOUNT)].value,
+                    '{%номер%}': row[self.get_col_index(self.StatementCols.NUMBER)].value,
+                    '{%имя%}': row[self.get_col_index(self.StatementCols.NAME)].value,
+                    '{%лицевой_счет%}': row[self.get_col_index(self.StatementCols.ACCOUNT)].value,
                     '{%месяц%}': 'Февраль',
                     '{%год%}': '2021',
-                    '{%долг%}': row[cls.get_col_index(cls.StatementCols.DEBT)].value,
+                    '{%долг%}': row[self.get_col_index(self.StatementCols.DEBT)].value,
                     '{%долг_рубли%}': '4043',
                     '{%долг_копейки%}': '00',
                 }
-                result.append(context)
-
-        return result
+                self.bill_data.append(context)
 
 
 if __name__ == '__main__':
     config = configparser.RawConfigParser()
     config.read(CONFIG_NAME, encoding="utf-8")
+    structure_conf = config['STRUCTURE']
 
-    STATEMENT_FOLDER = config['DEFAULT']['STATEMENT_FOLDER']
+    STATEMENT_FOLDER = structure_conf['STATEMENT_FOLDER']
 
-    TEMPLATE_FOLDER = config['DEFAULT']['TEMPLATE_FOLDER']
-    TEMPLATE_FILENAME = config['DEFAULT']['TEMPLATE_FILENAME']
+    TEMPLATE_FOLDER = structure_conf['TEMPLATE_FOLDER']
+    TEMPLATE_FILENAME = structure_conf['TEMPLATE_FILENAME']
 
-    OUTPUT_FOLDER = config['DEFAULT']['OUTPUT_FOLDER']
-    OUTPUT_FILENAME_FORMAT = config['DEFAULT']['OUTPUT_FILENAME_FORMAT']
+    OUTPUT_FOLDER = structure_conf['OUTPUT_FOLDER']
+    OUTPUT_FILENAME_FORMAT = structure_conf['OUTPUT_FILENAME_FORMAT']
 
-    FIRST_ROW = int(config['DEFAULT']['FIRST_ROW'])
-    LAST_ROW = int(config['DEFAULT']['LAST_ROW'])
+    FIRST_ROW = int(structure_conf['FIRST_ROW'])
+    LAST_ROW = int(structure_conf['LAST_ROW'])
+
+    cols_conf = config['COLUMNS']
+
+    BillGenerator.StatementCols.NAME = cols_conf['NAME']
+    BillGenerator.StatementCols.NUMBER = cols_conf['NUMBER']
+    BillGenerator.StatementCols.DEBT = cols_conf['DEBT']
+    BillGenerator.StatementCols.ACCOUNT = cols_conf['ACCOUNT']
+
     try:
-        bill_generator = BillGenerator()
-        result = bill_generator.read_statement()
-
         template_filename = os.path.join(TEMPLATE_FOLDER, TEMPLATE_FILENAME)
         template_wb = load_workbook(filename=template_filename)
-        for context in result:
-            bill_generator.fill_template(context, template_wb)
+
+        bill_generator = BillGenerator(template_wb)
+
+        for context in bill_generator.bill_data:
+            bill_generator.fill_template(context)
     except PermissionError:
         print("Произошла ошибка. Закройте все файлы Excel перед запуском.")
     except:
